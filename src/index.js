89549700c619a1eb26b616ac08022d7f03613aec
Bence Kormos <bence.kormos@merapar.com>
2021-07-19 09:38:15 +0100

    setup changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { Launch } from '@lightningjs/sdk'
 import App from './App.js'
 
-export default function() {
-  return Launch(App, ...arguments)
+export default function(appSettings, platformSettings) {
+  appSettings.stage.w = window.innerWidth
+  appSettings.stage.h = (window.innerWidth / 16) * 9
+  appSettings.stage.useImageWorker = false
+  console.log(appSettings.stage)
+  return Launch(App, appSettings, platformSettings)
 }
