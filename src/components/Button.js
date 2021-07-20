c72b9833da64bb64799c1fc4c49baa05d56174f6
Bence Kormos <bence.kormos@merapar.com>
2021-07-20 12:15:54 +0100

    button focus handling

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,11 +7,26 @@ export class Button extends Lightning.Component {
       w: 200,
       h: 50,
       color: 0xffff0000,
+      alpha: 0.8,
       Label: {
         text: {
           text: 'Button',
         },
       },
     }
+  }
+
+  _focus() {
+    this.patch({
+      color: 0xffffff00,
+      alpha: 1,
+    })
+  }
+
+  _unfocus() {
+    this.patch({
+      color: 0xffff0000,
+      alpha: 0.8,
+    })
   }
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,7 +5,7 @@ export class Dashboard extends Lightning.Component {
   static _template() {
     return {
       rect: true,
-      w: 500,
+      w: 1000,
       h: 500,
       color: 0x88000000,
       Text: {
@@ -15,24 +15,45 @@ export class Dashboard extends Lightning.Component {
           fontSize: 26,
         },
       },
-      Button1: {
-        type: Button,
-      },
-      Button2: {
-        type: Button,
-        // x: 20,
-        // y: 10,
-        mount: -1,
+      Buttons: {
+        Button1: {
+          type: Button,
+        },
+        Button2: {
+          type: Button,
+          mount: -1,
+        },
+        Button3: {
+          type: Button,
+          mount: -2,
+        },
+        Button4: {
+          type: Button,
+          mount: -3,
+        },
       },
     }
+  }
+
+  _getFocused() {
+    return this.tag('Buttons').children[this.index]
+  }
+
+  _init() {
+    this.index = 0
   }
 
   _handleBack() {
     Router.navigate('/splash')
   }
 
-  // _init() {
-  //   this.tag('Text')
-  //   this.tag('Button')
-  // }
+  _handleUp() {
+    const nextIndex = this.index + 1
+    this.index = this.tag('Buttons').children.length > nextIndex ? nextIndex : 0
+  }
+
+  _handleDown() {
+    const nextIndex = this.index - 1
+    this.index = nextIndex >= 0 ? nextIndex : this.tag('Buttons').children.length - 1
+  }
 }
